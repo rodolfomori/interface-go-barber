87b5d42e4175db86418a386aaca61e382a4676b8
Rodolfo <rod_dolpho@hotmail.com>
2019-11-21 19:45:37 -0300

    added notifications

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
-function handleSubmit(data) {
-  console.tron.log(data);
-}
+import { signUpRequest } from '~/store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('O nome é obrigatório'),
@@ -20,6 +19,12 @@ const schema = Yup.object().shape({
 });
 
 export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
   return (
     <>
       <img src={logo} alt="logo gobarber" />
